1f3e0608b3a9e6e08dbabe7a59246a3476ad0106
kalif7722 <38307808+kalif7722@users.noreply.github.com>
2026-09-08 10:25:29 +0530

    Add ML Engineer Associate course page

---
+++ app/courses/aws-machine-learning-engineer-associate/page.tsx
@@ -0,0 +1,3 @@
+import CertificationCourse from "../../components/CertificationCourse";
+import { mlaScope } from "../../course-data-extra";
+export default function Course(){return <CertificationCourse code="MLA" level="Associate" title="AWS Certified Machine Learning Engineer – Associate" description="Study machine-learning workloads and supporting AWS services using the official MLA-C01 certification scope." scope={mlaScope} sourceUrl="https://docs.aws.amazon.com/aws-certification/latest/machine-learning-engineer-associate-01/mla-01-in-scope-services.html" sourceLabel="Aligned to the AWS Certified Machine Learning Engineer – Associate MLA-C01 guide"/>;}
