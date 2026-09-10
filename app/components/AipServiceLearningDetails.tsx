60ae101f18bf143180d7231a3c0af8f230799d7c
kalif7722 <38307808+kalif7722@users.noreply.github.com>
2026-09-10 17:10:33 +0530

    Activate AIP V6 service-specific architecture pages

--- app/components/AipServiceLearningDetails.tsx
+++ app/components/AipServiceLearningDetails.tsx
@@ -1,1 +1,1 @@
-export { default, aipDetailServices, hasAipLearningDetails, getAipLearningMeta } from "./AipServiceLearningDetailsV5";
+export { default, aipDetailServices, hasAipLearningDetails, getAipLearningMeta } from "./AipServiceLearningDetailsV6";
