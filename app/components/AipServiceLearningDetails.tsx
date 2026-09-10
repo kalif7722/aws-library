6dd137847de97ae608805b97ae828e12f5ab5787
kalif7722 <38307808+kalif7722@users.noreply.github.com>
2026-09-10 17:41:14 +0530

    Activate complete AIP V7 architecture renderer

--- app/components/AipServiceLearningDetails.tsx
+++ app/components/AipServiceLearningDetails.tsx
@@ -1,1 +1,1 @@
-export { default, aipDetailServices, hasAipLearningDetails, getAipLearningMeta } from "./AipServiceLearningDetailsV6";
+export { default, aipDetailServices, hasAipLearningDetails, getAipLearningMeta } from "./AipServiceLearningDetailsV7";
